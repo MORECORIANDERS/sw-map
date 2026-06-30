7d8f74db6770ebb09bd25c9b8f9f027f6dfb2057
Ubuntu <ubuntu@localhost.localdomain>
2026-06-30 19:02:31 +0800

    sw-map: API 数据源改为 CloudBase api-bridge，支持实时数据更新

---
+++ js/apiConfig.js
@@ -0,0 +1,9 @@
+/**
+ * API 配置 - 可转债行业思维导图
+ * 数据源：CloudBase api-bridge 云函数
+ */
+var API_CONFIG = {
+  baseUrl: 'https://extracilantro.cn/api-bridge',
+  token: 'scbe2024',
+  dataPath: '/api/sw-map-data',
+};
